ac5319affa4f426ff9ad4d9c33d18cb1eb747dce
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-05-08 22:53:01 +0530

    utils.py to save csv to parquet

---
+++ src/utils.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+def list_files(path,filetype):
+    folder = Path(path)
+    files,names = [],[]
+    for file in folder.glob(f"*{filetype}"):
+        files.append(file.name)
+        names.append(file.stem)
+    return dict(zip(files,names))
+
+def csv_to_prqt(csv_path, output_dir):
+    try:
+        df = pd.read_csv(csv_path)
+    except UnicodeDecodeError:
+        print(f"UTF-8 decode failed for {csv_path}, trying 'latin1' encoding...")
+        df = pd.read_csv(csv_path, encoding='latin1')
+        
+    df.to_parquet(output_dir)    
+    print(f"converted {csv_path} to parquet")
+
+def main():
+    files = list_files('data/raw','csv')
+
+    for i,j in files.items():
+        input_path = f"data/raw/{i}"
+        output_path = f"data/cleaned/{j.lower()}.parquet"
+        csv_to_prqt(input_path,output_path)
+
+if __name__ == "__main__":
+    main()
