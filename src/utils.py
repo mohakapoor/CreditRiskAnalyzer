8cde2e07de31e00d480576c806942a6688e96a34
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-05-10 17:28:09 +0530

    updated utils.py to include downcasting

--- src/utils.py
+++ src/utils.py
@@ -17,8 +17,25 @@ def csv_to_prqt(csv_path, output_dir):
         print(f"UTF-8 decode failed for {csv_path}, trying 'latin1' encoding...")
         df = pd.read_csv(csv_path, encoding='latin1')
         
+    df = data_downcasting(df)
     df.to_parquet(output_dir)    
     print(f"converted {csv_path} to parquet")
+
+def data_downcasting(df):
+    for col in _df.columns:
+        col_type = df[col].dtype
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+
+            # Downcasting float64 to float32
+            if str(col_type).find('float') >= 0 and c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                df[col] = df[col].astype(np.float32)
+
+            # Downcasting int64 to int32
+            elif str(col_type).find('int') >= 0 and c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                df[col] = df[col].astype(np.int32)
+    return df
 
 def main():
     pass
